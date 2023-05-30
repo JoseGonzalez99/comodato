7211fe256663638d1a9ff580fd2d872f9463f6bd
fred <fsaldivarpy@gmail.com>
2023-05-30 10:43:35 -0400

    dockerfile y entrypoint.sh eliminados del directorio raiz

--- Package/comodato/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "The application will start in ${START_SLEEP}s..." && sleep ${START_SLEEP}
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/invoicecomodato.jar" "$@"
